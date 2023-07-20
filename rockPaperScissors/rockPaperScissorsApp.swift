8b3f479a6a9b87d0e8126193c900bc228e021d1a
MargaritaUsova <askacziaa@gmail.com>
2023-07-20 15:12:38 +0300

    Initial Commit

---
+++ rockPaperScissors/rockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorsApp.swift
+//  rockPaperScissors
+//
+//  Created by Margarita Usova on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
